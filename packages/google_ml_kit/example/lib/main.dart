5d6a5f43017b77a568594832f5ffc3a364cef06c
Muhammad Hafidh Firmansyah <sesudah.es@gmail.com>
2023-04-09 13:12:05 +0700

    fix face detect

--- packages/google_ml_kit/example/lib/main.dart
+++ packages/google_ml_kit/example/lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-import 'nlp_detector_views/entity_extraction_view.dart';
-import 'nlp_detector_views/language_identifier_view.dart';
-import 'nlp_detector_views/language_translator_view.dart';
-import 'nlp_detector_views/smart_reply_view.dart';
-import 'vision_detector_views/barcode_scanner_view.dart';
-import 'vision_detector_views/digital_ink_recognizer_view.dart';
+
 import 'vision_detector_views/face_detector_view.dart';
-import 'vision_detector_views/label_detector_view.dart';
-import 'vision_detector_views/object_detector_view.dart';
-import 'vision_detector_views/pose_detector_view.dart';
-import 'vision_detector_views/selfie_segmenter_view.dart';
-import 'vision_detector_views/text_detector_view.dart';
+
 
 List<CameraDescription> cameras = [];
 
@@ -53,29 +44,15 @@ class Home extends StatelessWidget {
                   ExpansionTile(
                     title: const Text('Vision APIs'),
                     children: [
-                      CustomCard('Barcode Scanning', BarcodeScannerView()),
+ 
                       CustomCard('Face Detection', FaceDetectorView()),
-                      CustomCard('Image Labeling', ImageLabelView()),
-                      CustomCard('Object Detection', ObjectDetectorView()),
-                      CustomCard('Text Recognition', TextRecognizerView()),
-                      CustomCard('Digital Ink Recognition', DigitalInkView()),
-                      CustomCard('Pose Detection', PoseDetectorView()),
-                      CustomCard('Selfie Segmentation', SelfieSegmenterView()),
+
                     ],
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  ExpansionTile(
-                    title: const Text('Natural Language APIs'),
-                    children: [
-                      CustomCard('Language ID', LanguageIdentifierView()),
-                      CustomCard(
-                          'On-device Translation', LanguageTranslatorView()),
-                      CustomCard('Smart Reply', SmartReplyView()),
-                      CustomCard('Entity Extraction', EntityExtractionView()),
-                    ],
-                  ),
+             
                 ],
               ),
             ),
--- packages/google_ml_kit/example/lib/vision_detector_views/face_detector_view.dart
+++ packages/google_ml_kit/example/lib/vision_detector_views/face_detector_view.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
@@ -50,22 +52,46 @@ class _FaceDetectorViewState extends State<FaceDetectorView> {
       _text = '';
     });
     final faces = await _faceDetector.processImage(inputImage);
-    if (inputImage.inputImageData?.size != null &&
-        inputImage.inputImageData?.imageRotation != null) {
-      final painter = FaceDetectorPainter(
-          faces,
-          inputImage.inputImageData!.size,
-          inputImage.inputImageData!.imageRotation);
-      _customPaint = CustomPaint(painter: painter);
-    } else {
-      String text = 'Faces found: ${faces.length}\n\n';
-      for (final face in faces) {
-        text += 'face: ${face.boundingBox}\n\n';
+ 
+
+      for (Face face in faces) {
+        final Rect boundingBox = face.boundingBox;
+
+        final double? rotX = face.headEulerAngleX; // Head is tilted up and down rotX degrees
+        final double? rotY = face.headEulerAngleY; // Head is rotated to the right rotY degrees
+        final double? rotZ = face.headEulerAngleZ; // Head is tilted sideways rotZ degrees
+
+        // If landmark detection was enabled with FaceDetectorOptions (mouth, ears,
+        // eyes, cheeks, and nose available):
+        print("Kepala atas bawah ${face.headEulerAngleX}");
+        print("Kepala kanan kiri ${face.headEulerAngleY}");
+        print("Kepala tilt ${face.headEulerAngleZ}");
+        // If classification was enabled with FaceDetectorOptions:
+        if (face.smilingProbability != null) {
+          final double? smileProb = face.smilingProbability;
+        }
+
+        // If face tracking was enabled with FaceDetectorOptions:
+        if (face.trackingId != null) {
+          final int? id = face.trackingId;
+        }
       }
-      _text = text;
-      // TODO: set _customPaint to draw boundingRect on top of image
-      _customPaint = null;
-    }
+    // if (inputImage.inputImageData?.size != null &&
+    //     inputImage.inputImageData?.imageRotation != null) {
+    //   final painter = FaceDetectorPainter(
+    //       faces,
+    //       inputImage.inputImageData!.size,
+    //       inputImage.inputImageData!.imageRotation);
+    //   _customPaint = CustomPaint(painter: painter);
+    // } else {
+    //   String text = 'Faces found: ${faces.length}\n\n';
+    //   for (final face in faces) {
+    //     text += 'face: ${face.boundingBox}\n\n';
+    //   }
+    //   _text = text;
+    //   // TODO: set _customPaint to draw boundingRect on top of image
+    //   _customPaint = null;
+    // }
     _isBusy = false;
     if (mounted) {
       setState(() {});
